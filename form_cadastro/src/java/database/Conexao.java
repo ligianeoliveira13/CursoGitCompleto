2b94e1fae1d5d8c1cba1b01b9ded8a788578e4d1
Ligiane Mara de Oliveira <44845315+ligianeoliveira13@users.noreply.github.com>
2019-01-27 18:18:03 -0200

    Alteração do comentario no site

--- form_cadastro/src/java/database/Conexao.java
+++ form_cadastro/src/java/database/Conexao.java
@@ -14,7 +14,7 @@ public class Conexao {
     private String host;
     private Connection connection = null;
  
-    //construtor
+    //construtor++++++
     public Conexao(String host, String database, String user, String password) {
  
         this.host = host; //geralmente localhost
